e8179020529ee5337c445578c68d72b4429357f0
Shady <ShadyZekry75@gmail.com>
2021-06-07 13:49:34 +0200

    Refactor graphql methods

--- lib/fly.dart
+++ lib/fly.dart
@@ -23,59 +23,45 @@ class Fly<T> {
 
   Future<Map<String, dynamic>> query(
     List<Node> querys, {
-    Map<String, dynamic> qParams,
-    Map<String, dynamic> parsers,
-    String apiURL,
-    Map<String, String> parameters,
-  }) async {
-    Node mainQuery = Node(name: 'query', cols: querys);
-
-    Map queryMap = {
-      "operationName": null,
-      "variables": {},
-      "query": GraphQB(mainQuery).getQueryFor(args: qParams)
-    };
-
-    Map<String, dynamic> results = await this.requestWithoutParse(
-        query: queryMap, apiUrl: apiURL, parameters: parameters);
-
-    if (parsers != null) return _parseResults(results, parsers);
-
-    return results;
-  }
-
-  Map<String, dynamic> _parseResults(
-      Map<String, dynamic> results, Map<String, dynamic> parsers) {
-    try {
-      return results.map((key, value) {
-        if (!parsers.containsKey(key)) return MapEntry(key, value);
-
-        if (value is List) {
-          return MapEntry(key, parsers[key].dynamicParse(value));
-        } else {
-          print("INSIDE IS NOOOT " + key);
-          return MapEntry(key, parsers[key].parse(value));
-        }
-      });
-    } catch (e) {
-      print(e.toString());
-      return {};
-    }
-  }
-
-  void addHeaders(Map<String, String> headers) {
-    _apiManager.setHeaders(headers);
-    _webAPIManager.setHeaders(headers);
-  }
+    Map<String, dynamic>? qParams,
+    Map<String, dynamic>? parsers,
+    String? apiURL,
+    Map<String, String>? parameters,
+  }) async =>
+      await graphMethod(
+        methodName: 'mutation',
+        mainQueryCols: querys,
+        qParams: qParams,
+        parsers: parsers,
+        apiURL: apiURL,
+        parameters: parameters,
+      );
 
   Future<Map<String, dynamic>> mutation(
     List<Node> mutations, {
-    Map<String, dynamic> qParams,
-    Map<String, Parser<T>> parsers,
-    String apiURL,
-    Map<String, String> parameters,
+    Map<String, dynamic>? qParams,
+    Map<String, dynamic>? parsers,
+    String? apiURL,
+    Map<String, String>? parameters,
+  }) async =>
+      await graphMethod(
+        methodName: 'mutation',
+        mainQueryCols: mutations,
+        qParams: qParams,
+        parsers: parsers,
+        apiURL: apiURL,
+        parameters: parameters,
+      );
+
+  Future<Map<String, dynamic>> graphMethod({
+    required String methodName,
+    List<Node>? mainQueryCols,
+    Map<String, dynamic>? qParams,
+    Map<String, dynamic>? parsers,
+    String? apiURL,
+    Map<String, String>? parameters,
   }) async {
-    Node mainQuery = Node(name: 'mutation', cols: mutations);
+    Node mainQuery = Node(name: methodName, cols: mainQueryCols);
 
     Map queryMap = {
       "operationName": null,
@@ -84,10 +70,24 @@ class Fly<T> {
     };
     Map<String, dynamic> results = await this.requestWithoutParse(
         query: queryMap, apiUrl: apiURL, parameters: parameters);
-    if (parsers == null || parsers.length == 0 || results == null)
-      return results;
+
+    if (parsers == null || parsers.length == 0) return results;
 
     return _parseResults(results, parsers);
+  }
+
+  Future<T> request({
+    String? apiUrl,
+    required dynamic query,
+    Map<String, String>? parameters,
+    Parser<T>? parser,
+  }) async {
+    Map<String, dynamic> results = await this.requestWithoutParse(
+        query: query, apiUrl: apiUrl, parameters: parameters);
+
+    if (parser == null) return results as T;
+
+    return parser.parse(results);
   }
 
   Future<Map<String, dynamic>> requestWithoutParse(
@@ -136,62 +136,33 @@ class Fly<T> {
     return myData['data'];
   }
 
-  Future<T> request({
-    String apiUrl,
-    dynamic query,
-    Map<String, String> parameters,
-    Parser<T> parser,
-  }) async {
-    if (_apiURL == null && apiUrl == null) {
-      throw ("apiUrl is not set! call init or add apiUrl in request");
+  Map<String, dynamic> _parseResults(
+      Map<String, dynamic> results, Map<String, dynamic> parsers) {
+    try {
+      return results.map((key, value) {
+        if (!parsers.containsKey(key)) return MapEntry(key, value);
+
+        if (value is List) {
+          return MapEntry(key, parsers[key].dynamicParse(value));
+        } else {
+          print("INSIDE IS NOOOT " + key);
+          return MapEntry(key, parsers[key].parse(value));
+        }
+      });
+    } catch (e) {
+      print(e.toString());
+      return {};
     }
-    if (apiUrl == null) {
-      apiUrl = _apiURL;
-    }
-    if (parameters == null) {
-      parameters = defaultParams;
-    }
+  }
 
-    Response response;
-    if (kIsWeb) {
-      response = await _webAPIManager.post(
-        apiUrl,
-        body: jsonEncode(query),
-      );
-    } else {
-      response = await _apiManager.post(
-        apiUrl,
-        body: jsonEncode(query),
-      );
-    }
-
-    Map<String, dynamic> myData = json.decode(response.body);
-
-    // has error
-    if (myData.containsKey("errors")) {
-      String error = myData['errors'][0]['message'];
-      String trace = myData['errors'][0]['trace'].toString();
-      int code = myData['errors'][0]['extensions']['code'];
-
-      throw AppException(true,
-          beautifulMsg: error,
-          name: "Server Error",
-          code: code,
-          uglyMsg: trace);
-    }
-
-    if (myData["data"] == null) {
-      return null;
-    }
-
-    final result = parser.parse(myData['data']);
-
-    return result;
+  void addHeaders(Map<String, String> headers) {
+    _apiManager.setHeaders(headers);
+    _webAPIManager.setHeaders(headers);
   }
 }
 
 abstract class Parser<T> {
-  List<String> querys;
+  List<String>? querys;
 
   T parse(dynamic data);
 
